323b2fef185870ab02172e4ab5342d26a3b8bd73
ffnuser <151765508+FFNUser@users.noreply.github.com>
2026-03-22 00:12:12 +0500

    Update utils.sh

--- utils.sh
+++ utils.sh
@@ -186,6 +186,7 @@ config_update() {
 					fi
 				fi
 			fi
+		fi
 		fi_SRC=$(toml_get "$t" patches-source) || PATCHES_SRC=$DEF_PATCHES_SRC
 		PATCHES_VER=$(toml_get "$t" patches-version) || PATCHES_VER=$DEF_PATCHES_VER
 		if [[ -v sources["$PATCHES_SRC/$PATCHES_VER"] ]]; then
